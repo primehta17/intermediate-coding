864c684df93aed58698593eca296514382970dc2
Priyanka Mehta <primehta17@gmail.com>
2025-10-09 23:57:44 +0530

    max array sum

--- arrayquestion/maxarraysum.js
+++ arrayquestion/maxarraysum.js
@@ -13,3 +13,19 @@ function subarray(arr) {
   return max;
 }
 console.log(subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+function maxarraysum(arr) {
+  let sum = 0;
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (sum > max) {
+      max = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return max;
+}
+console.log(maxarraysum([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
