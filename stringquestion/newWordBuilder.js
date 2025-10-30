35797074f5bb4cd6b7e366675a0997e90e508fa5
Priyanka Mehta <primehta17@gmail.com>
2025-10-30 23:49:33 +0530

    newWordBuilder

--- stringquestion/newWordBuilder.js
+++ stringquestion/newWordBuilder.js
@@ -6,3 +6,14 @@ function newWordBuilder(word) {
 }
 console.log(newWordBuilder("challenge"));
 console.log(newWordBuilder("hi"));
+
+function newWordBuilder(word) {
+  if (word.length < 2) {
+    return word; // or return "" if that fits better
+  }
+  return word.slice(0, 2) + word.slice(-2);
+}
+
+console.log(newWordBuilder("challenge")); // “chge”
+console.log(newWordBuilder("hi")); // “hi”
+console.log(newWordBuilder("a")); // “a”   (since length < 2)
