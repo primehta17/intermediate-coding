f1948d06c8daaff13b016940ebe2b7e59830061f
Priyanka Mehta <primehta17@gmail.com>
2025-11-02 23:23:24 +0530

    route

--- top10concept/route.js
+++ top10concept/route.js
@@ -11,9 +11,16 @@ router.get("/", (req, res) => {
   };
   res.json(obj);
 });
+router.get("/", (req, res) => {
+  res.json({ message: "Welcome to User Route" });
+});
+
+router.post("/add", (req, res) => {
+  res.json({ message: "User added successfully" });
+});
 
 // Mount the router
-app.use("/", router);
+app.use("/", router); //use it in routes
 
 // Start server
 app.listen(7000, () => {
