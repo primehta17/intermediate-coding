e2a6c6e50a77d4ba170822b74af7af78c0fedce1
Priyanka Mehta <primehta17@gmail.com>
2025-12-03 18:18:34 +0530

    push zeros

--- numberquestion/pushzero.js
+++ numberquestion/pushzero.js
@@ -13,3 +13,18 @@ function movezero(arr) {
   return newarr;
 }
 console.log(movezero([6, 0, 3, 2, 0, 2, 0]));
+
+function movezeros(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      // let temp = arr[i];
+      // arr[i] = arr[count];
+      // arr[count] = temp;
+      [arr[i], arr[count]] = [arr[count], arr[i]]; //swap
+      count++;
+    }
+  }
+  return arr;
+}
+console.log(movezeros([6, 0, 3, 2, 0, 2, 0]));
